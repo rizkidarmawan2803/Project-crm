5052887992d586431fd8fc4f1d71833ba04116c1
Faris <accountmac@Faris-MacBook-Air.local>
2026-05-10 10:02:59 +0700

    feat: Menambahkan halaman Login

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
