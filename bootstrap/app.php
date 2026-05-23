ad8bc17038b4200e7fe10f20b4cb787337fcb996
rizkidarmawan2803 <riskidarmawan991@gmail.com>
2026-05-23 21:30:10 +0700

    memperbaiki tampilan dashboard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
 
     ->withMiddleware(function (Middleware $middleware) {
-        
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        ]);
 
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
@@ -25,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
+
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
